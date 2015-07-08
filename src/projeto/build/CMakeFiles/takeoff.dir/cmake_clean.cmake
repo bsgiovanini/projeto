2f5d1ddadfcd8e83e847b9bfe57ed6b165fcfb10
Bruno <bsgiovanini@gmail.com>
2015-07-07 23:21:28 -0300

    initial commit

---
+++ src/projeto/build/CMakeFiles/takeoff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/takeoff.dir/src/takeoff.o"
+  "../bin/takeoff.pdb"
+  "../bin/takeoff"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/takeoff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
