92af32139095a2db0d84787a05093a8d9f07fc31
Bruno <bsgiovanini@gmail.com>
2015-07-16 19:36:59 -0300

    incio do octamap

--- src/projeto/src/takeoff.cpp
+++ src/projeto/src/takeoff.cpp
@@ -25,37 +25,119 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define M_PI 3.14159265358979323846
+
 // %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <ardrone_autonomy/Navdata.h>
 #include <sensor_msgs/Range.h>
+#include <octomap/octomap.h>
+#include <octomap/OcTree.h>
+#include <math.h>
+#include <eigen3/Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+using namespace octomap;
 
 double vx_=0.0;
 double vy_=0.0;
 double vz_=0.0;
-float timestamp_anterior = 0.0;
+
+int row = 0;
+int pitch = 0;
+int yaw = 0;
+
+float previous_tm = 0.0;
+OcTree tree (0.1);  // create empty tree with resolution 0.1
+
+Vector3d x(0,0,0);// posicao linear inicial
+
+Matrix3d rotation(Vector3d theta) {
+
+    AngleAxisd rollAngle(theta(0), Vector3d::UnitX());
+    AngleAxisd pitchAngle(theta(1), Vector3d::UnitY());
+    AngleAxisd yawAngle(theta(2), Vector3d::UnitZ());
+
+    Quaternion<double> q = rollAngle * pitchAngle * yawAngle;
+
+    return q.matrix();
+}
+
+double degree_to_rad(int degrees) {
+    return M_PI / 180 * degrees;
+
+}
 
 void sonar_front_callback(const sensor_msgs::Range& msg_in)
 {
 	//ROS_INFO("getting sensor reading");
-	ROS_INFO("Range: [%f]", msg_in.range);
+    ROS_INFO("Range: [%f]", msg_in.range);
+
+    cout << endl;
+    cout << "generating example map" << endl;
+
+    // insert some measurements of occupied cells
+
+    for (int x=-20; x<20; x++) {
+        for (int y=-20; y<20; y++) {
+            for (int z=-20; z<20; z++) {
+                point3d endpoint ((float) x*0.05f, (float) y*0.05f, (float) z*0.05f);
+                tree.updateNode(endpoint, true); // integrate 'occupied' measurement
+            }
+        }
+    }
+
+    // insert some measurements of free cells
+
+    for (int x=-30; x<30; x++) {
+        for (int y=-30; y<30; y++) {
+            for (int z=-30; z<30; z++) {
+                point3d endpoint ((float) x*0.02f-1.0f, (float) y*0.02f-1.0f, (float) z*0.02f-1.0f);
+                tree.updateNode(endpoint, false);  // integrate 'free' measurement
+            }
+        }
+    }
+
+    point3d query (0., 0., 0.);
+    OcTreeNode* result = tree.search (query);
+
+
+    query = point3d(-1.,-1.,-1.);
+    result = tree.search (query);
+
+
+    query = point3d(1.,1.,1.);
+    result = tree.search (query);
+
 
 }
 
 void nav_callback(const ardrone_autonomy::Navdata& msg_in)
 {
-	//Take in state of ardrone
 
-    float dt = msg_in.tm - timestamp_anterior;
+    //timestamp in microsecs
+    float dt = (msg_in.tm - previous_tm)/1000000; //geting dt in secs
 
 	vx_=msg_in.vx*0.001;
 	vy_=msg_in.vy*0.001;
 	vz_=msg_in.vz*0.001;
+
+	Vector3d theta(degree_to_rad(msg_in.rotX), degree_to_rad(msg_in.rotY), degree_to_rad(msg_in.rotZ));
+
+	Matrix3d R = rotation(theta);
+
+	Vector3d velV (vx_, vy_, vz_);
+
+	Vector3d vel = R * velV;
+
+    x = x + vel*dt;
+
 	//ROS_INFO("getting sensor reading");
-	//ROS_INFO("I heard vx: [%f]  vy: [%f] vz: [%f]", vx_, vy_, vz_);
-    ROS_INFO("Time: [%f]", dt);
-    timestamp_anterior = msg_in.tm;
+	ROS_INFO("I heard x: [%f]  y: [%f] z: [%f]", x(0), x(1), x(2));
+    //ROS_INFO("Time: [%f]", dt);
+    previous_tm = msg_in.tm;
 }
 // %EndTag(CALLBACK)%
 
