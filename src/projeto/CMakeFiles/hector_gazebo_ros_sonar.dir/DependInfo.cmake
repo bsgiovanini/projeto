540153a94089c37ff6cb9822b7a04c03313dc47f
Bruno <bsgiovanini@gmail.com>
2015-07-17 20:53:59 -0300

    visualizacao do mapa

---
+++ src/projeto/CMakeFiles/hector_gazebo_ros_sonar.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bruno/ros_workspace/src/projeto/src/gazebo_ros_sonar.cpp" "/home/bruno/ros_workspace/src/projeto/CMakeFiles/hector_gazebo_ros_sonar.dir/src/gazebo_ros_sonar.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
--- src/projeto/src/takeoff.cpp
+++ src/projeto/src/takeoff.cpp
@@ -26,6 +26,7 @@
  */
 
 #define M_PI 3.14159265358979323846
+#define MAX_RANGE 3.0
 
 // %Tag(FULLTEXT)%
 #include "ros/ros.h"
@@ -37,6 +38,11 @@
 #include <math.h>
 #include <eigen3/Eigen/Dense>
 
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
 using namespace Eigen;
 using namespace std;
 using namespace octomap;
@@ -45,16 +51,15 @@ double vx_=0.0;
 double vy_=0.0;
 double vz_=0.0;
 
-int row = 0;
-int pitch = 0;
-int yaw = 0;
+Vector3d theta(0,0,0);
 
 float previous_tm = 0.0;
-OcTree tree (0.1);  // create empty tree with resolution 0.1
+OcTree tree (0.2);  // create empty tree with resolution 0.1
 
-Vector3d x(0,0,0);// posicao linear inicial
+Vector3d x(0,0,0);// global pose quadrotor
+Vector3d s_front_rel_pose;
 
-Matrix3d rotation(Vector3d theta) {
+Quaternion<double> rotation(Vector3d theta) {
 
     AngleAxisd rollAngle(theta(0), Vector3d::UnitX());
     AngleAxisd pitchAngle(theta(1), Vector3d::UnitY());
@@ -62,7 +67,7 @@ Matrix3d rotation(Vector3d theta) {
 
     Quaternion<double> q = rollAngle * pitchAngle * yawAngle;
 
-    return q.matrix();
+    return q;
 }
 
 double degree_to_rad(int degrees) {
@@ -70,17 +75,36 @@ double degree_to_rad(int degrees) {
 
 }
 
+void load_sonar_rel_transform_m() {
+
+    Vector3d sonar_f_rel_linear_pos(0.1, 0.0, 0.12);
+    Vector3d sonar_f_rel_rot_pos(0, 0, 0);
+    s_front_rel_pose = rotation(sonar_f_rel_rot_pos).matrix() * sonar_f_rel_linear_pos;
+
+}
+
 void sonar_front_callback(const sensor_msgs::Range& msg_in)
 {
-	//ROS_INFO("getting sensor reading");
-    ROS_INFO("Range: [%f]", msg_in.range);
+	ROS_INFO("Range: [%f]", msg_in.range);
 
-    cout << endl;
-    cout << "generating example map" << endl;
+    Vector3d global_s_front_pose = x + rotation(theta).matrix() * s_front_rel_pose;
+    ROS_INFO("I heard sx: [%f]  sy: [%f] sz: [%f]", global_s_front_pose(0), global_s_front_pose(1), global_s_front_pose(2));
+
+    point3d startPoint ((float) global_s_front_pose(0), (float) global_s_front_pose(1), (float) global_s_front_pose(2));
+
+    Vector3d global_end_ray = global_s_front_pose + rotation(theta).matrix() * Vector3d(msg_in.range, 0, 0);
+
+    ROS_INFO("I heard rayx: [%f]  rayy: [%f] rayz: [%f]", global_end_ray(0), global_end_ray(1), global_end_ray(2));
+
+    point3d endPoint ((float) global_end_ray(0), (float) global_end_ray(1), (float) global_end_ray(2));
+
+    if (msg_in.range < MAX_RANGE) {
+        tree.insertRay (startPoint, endPoint, MAX_RANGE);
+     }
 
     // insert some measurements of occupied cells
 
-    for (int x=-20; x<20; x++) {
+/*    for (int x=-20; x<20; x++) {
         for (int y=-20; y<20; y++) {
             for (int z=-20; z<20; z++) {
                 point3d endpoint ((float) x*0.05f, (float) y*0.05f, (float) z*0.05f);
@@ -110,6 +134,7 @@ void sonar_front_callback(const sensor_msgs::Range& msg_in)
 
     query = point3d(1.,1.,1.);
     result = tree.search (query);
+    */
 
 
 }
@@ -124,9 +149,11 @@ void nav_callback(const ardrone_autonomy::Navdata& msg_in)
 	vy_=msg_in.vy*0.001;
 	vz_=msg_in.vz*0.001;
 
-	Vector3d theta(degree_to_rad(msg_in.rotX), degree_to_rad(msg_in.rotY), degree_to_rad(msg_in.rotZ));
+	theta(0) = degree_to_rad(msg_in.rotX);
+	theta(1) = degree_to_rad(msg_in.rotY);
+	theta(2) = degree_to_rad(msg_in.rotZ);
 
-	Matrix3d R = rotation(theta);
+	Matrix3d R = rotation(theta).matrix();
 
 	Vector3d velV (vx_, vy_, vz_);
 
@@ -134,16 +161,28 @@ void nav_callback(const ardrone_autonomy::Navdata& msg_in)
 
     x = x + vel*dt;
 
+
+
 	//ROS_INFO("getting sensor reading");
-	ROS_INFO("I heard x: [%f]  y: [%f] z: [%f]", x(0), x(1), x(2));
+	//
+	//ROS_INFO("I heard x: [%f]  y: [%f] z: [%f]", x(0), x(1), x(2));
+
     //ROS_INFO("Time: [%f]", dt);
     previous_tm = msg_in.tm;
 }
 // %EndTag(CALLBACK)%
 
+void my_handler(int s){
+    printf("Escrevendo no arquivo %d\n",s);
+    tree.writeBinary("simple_tree.bt");
+
+    exit(1);
+
+}
 
 int main(int argc, char **argv)
 {
+    load_sonar_rel_transform_m();
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -181,7 +220,7 @@ int main(int argc, char **argv)
 // %Tag(SUBSCRIBER)%
   ros::Subscriber sub_nav = n.subscribe("/ardrone/navdata", 1, nav_callback);
 // %EndTag(SUBSCRIBER)%
-//  ros::Subscriber sub_sensor = n.subscribe("/sonar_front", 1, sonar_front_callback);
+  ros::Subscriber sub_sensor = n.subscribe("/sonar_front", 1, sonar_front_callback);
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
    * callbacks will be called from within this thread (the main one).  ros::spin()
@@ -190,6 +229,16 @@ int main(int argc, char **argv)
 // %Tag(SPIN)%
   ros::spin();
 // %EndTag(SPIN)%
+
+  struct sigaction sigIntHandler;
+
+   sigIntHandler.sa_handler = my_handler;
+   sigemptyset(&sigIntHandler.sa_mask);
+   sigIntHandler.sa_flags = 0;
+
+   sigaction(SIGINT, &sigIntHandler, NULL);
+
+   pause();
 
   return 0;
 }
