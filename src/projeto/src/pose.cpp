e3b26303fe93ce054123dd7d17f5978e90735cc6
Bruno <bsgiovanini@gmail.com>
2015-09-07 22:38:25 -0300

    ekf

--- src/projeto/src/pose.cpp
+++ src/projeto/src/pose.cpp
@@ -32,6 +32,8 @@
 #include <unistd.h>
 #include <vector>
 
+
+
 using namespace Eigen;
 using namespace std;
 
@@ -39,6 +41,155 @@ using namespace std;
 ros::Publisher pub_enable_collision_mode, pub_vel;
 geometry_msgs::Twist twist;
 
+class EKF {
+    public:
+        Vector3d _pose;
+
+        double _last_yaw;
+
+        Matrix3d _sigma, _q, _r;
+
+
+        /*void predict_measure(Vector3d measure_pose_rel) {
+
+            double state_yaw = _pose(2);
+
+            double mx = measure_pose_rel(0);
+            double my = measure_pose_rel(1);
+
+
+
+        }*/
+
+        double rand_normal(double mean, double stddev) {
+            static double n2 = 0.0;
+            static int n2_cached = 0;
+            if (!n2_cached) {
+                double x, y, r;
+            do {
+                x = 2.0*rand()/RAND_MAX - 1;
+                y = 2.0*rand()/RAND_MAX - 1;
+
+                r = x*x + y*y;
+            } while (r == 0.0 || r > 1.0);
+                 {
+                 double d = sqrt(-2.0*log(r)/r);
+            double n1 = x*d;
+            n2 = y*d;
+                 double result = n1*stddev + mean;
+                 n2_cached = 1;
+                 return result;
+                 }
+             } else {
+                 n2_cached = 0;
+                 return n2*stddev + mean;
+             }
+         }
+
+        void predict_state(float dt, Vector3d v_local, Vector3d theta) {
+
+            //double roll = theta(0);
+            //double pitch = theta(1);
+            double yaw = theta(2);
+            double vx = v_local(0);
+            double vy = v_local(1);
+
+            if (_last_yaw == -9999999) {
+                _last_yaw = yaw;
+                return;
+            }
+
+            //cout << _last_yaw << " " << yaw << endl;
+
+            double o_dx = vx * dt;
+            double o_dy = vy * dt;
+            double o_dyaw = yaw - _last_yaw;
+
+//            cout << " yaw: " << _last_yaw << " " << yaw << endl;
+//            cout << " o: " << o_dx << " " << o_dy << " "  << o_dyaw <<  endl;
+
+            _last_yaw = yaw;
+
+            _pose(2) = yaw + o_dyaw;
+            _pose(2) = atan2(sin(_pose(2)), cos(_pose(2))); //normalize yaw
+
+            _pose(0) = _pose(0) + o_dx * cos(_pose(2)) - o_dy* sin(_pose(2));
+            _pose(1) = _pose(1) + o_dx * sin(_pose(2)) + o_dy* cos(_pose(2));
+
+            Matrix3d F;
+            F <<    1, 0, -sin(_pose(2)) * o_dx - cos(_pose(2)) * o_dy,
+                    0, 1,  cos(_pose(2)) * o_dx - sin(_pose(2)) * o_dy,
+                    0, 0, 1;
+
+            _sigma = F * (_sigma * (F.transpose())) + _q;
+
+
+        }
+
+        Vector3d h(Vector3d x, Vector3d m) {
+
+            Vector3d toReturn;
+            toReturn << x(0) + cos(x(2)) * m(0) - sin(x(2)) * m(1),
+                        x(1) + sin(x(2)) * m(0) + cos(x(2)) * m(1),
+                        x(2);
+            return toReturn;
+
+        }
+
+        void correct_state(Vector3d measure_pose_rel) {
+
+            double state_yaw = _pose(2);
+
+            double mx = measure_pose_rel(0);
+            double my = measure_pose_rel(1);
+
+            double random_number = rand_normal(0, 0.02);
+
+            Vector3d z = h(_pose, measure_pose_rel) + Vector3d(random_number, random_number, random_number);
+
+            Matrix3d H;
+            H <<    1, 0, -sin(state_yaw) * mx - cos(state_yaw) * my,
+                    0, 1,  cos(state_yaw) * mx - sin(state_yaw) * my,
+                    0, 0, 1;
+
+            Matrix3d Ht = H.transpose();
+
+            //calc Gain
+
+            Matrix3d G = _sigma * Ht * ((H * _sigma * Ht + _r).inverse());
+
+            //cout << z << " " << h(_pose, measure_pose_rel) << endl;
+
+            _pose = _pose + G*(z - h(_pose, measure_pose_rel));
+
+            _sigma = (Matrix3d::Identity() - G * H) * _sigma;
+
+
+        }
+
+        void reset() {
+
+            _pose = Vector3d::Zero();
+            _sigma = Matrix3d::Identity();
+
+            _q =  Matrix3d(Vector3d(0.0003, 0.0003, 0.0001).asDiagonal());
+            _r =  Matrix3d(Vector3d(0.0004, 0.0004, 0.0004).asDiagonal());
+            _last_yaw = -9999999;
+        }
+
+        EKF() {
+            reset();
+        }
+
+        Vector3d state() {
+            return _pose;
+        }
+
+
+};
+
+
+EKF ekf;
 
 Vector3d theta(0,0,0);
 Vector3d previous_theta(0,0,0);
@@ -47,6 +198,7 @@ Vector3d previous_vel(0,0,0);
 
 
 Vector3d x(0,0,0);// global pose quadrotor
+
 
 Vector3d s_front_rel_pose;
 
@@ -63,6 +215,8 @@ Quaternion<double> rotation(Vector3d theta) {
 
     return q;
 }
+
+
 
 Matrix3d angularTransformationMatrix(Vector3d angles) {
     float phi = angles(0);
@@ -108,16 +262,25 @@ void sonar_front_callback(const sensor_msgs::Range& msg_in)
 {
 	//ROS_INFO("Range: [%f]", msg_in.range);
 
-    //Vector3d global_s_front_pose = x + rotation(theta).matrix() * s_front_rel_pose;
+    Vector3d global_s_front_pose = x + rotation(theta).matrix() * s_front_rel_pose;
     //ROS_INFO("I heard sx: [%f]  sy: [%f] sz: [%f]", global_s_front_pose(0), global_s_front_pose(1), global_s_front_pose(2));
 
-    //Vector3d global_end_ray = global_s_front_pose + rotation(theta).matrix() * Vector3d(msg_in.range, 0, 0);
+    Vector3d global_end_ray = global_s_front_pose + rotation(theta).matrix() * Vector3d(msg_in.range, 0, 0);
 
     //ROS_INFO("I heard rayx: [%f]  rayy: [%f] rayz: [%f]", global_end_ray(0), global_end_ray(1), global_end_ray(2));
 
+    Vector3d rel_ray_pose = s_front_rel_pose + Vector3d(msg_in.range, 0, 0);
+
+    ekf.correct_state(rel_ray_pose);
+
+    cout << "odometria x_x: " <<  x(0) << " x_y: " << x(1) << endl;
+
+    Vector3d k_x = ekf.state();
+
+    cout << "kalman f  x_x: " <<  k_x(0) << " x_y: " << k_x(1) << endl;
+    //ROS_INFO("kalman f  x_x: [%f]  x_y: [%f]", k_x(0), k_x(1));
 }
 
-//void predict_state(float dt, Vector3d x_global, Vector3d v_local, )
 
 void nav_callback(const ardrone_autonomy::Navdata& msg_in)
 {
@@ -146,7 +309,9 @@ void nav_callback(const ardrone_autonomy::Navdata& msg_in)
 
     x = x + vel*dt;
 
-    ROS_INFO("odometria x_x: [%f]  x_y: [%f] x_z: [%f]", x(0), x(1), x(2));
+    ekf.predict_state(dt, velV, theta);
+
+
 
     Vector3d thetadot = (theta - previous_theta)/dt;
 
@@ -176,6 +341,7 @@ void nav_callback(const ardrone_autonomy::Navdata& msg_in)
 
 int main(int argc, char **argv)
 {
+
     load_sonar_rel_transform_m();
 
     ros::init(argc, argv, "listener");
