f2673e08287ea50ae30a81361b0f8fc6cb1d6f27
Bruno <bsgiovanini@gmail.com>
2016-03-18 16:48:11 -0300

    acc

--- src/projeto/src/pose.cpp
+++ src/projeto/src/pose.cpp
@@ -23,6 +23,8 @@
 #include <vector>
 #include <fstream>
 
+#include <deque>
+
 
 
 using namespace Eigen;
@@ -52,6 +54,20 @@ void f_vector_print(string name, Vector3d vectors) {
     cout << name << ": x: " << vectors(0) << ", y: " << vectors(1) << ", z: " << vectors(2) << endl;
 }
 
+struct sample {
+
+    Vector3d data;
+    unsigned long long time_us;
+};
+
+deque<sample> buffer_acc;
+
+#define ACC_BUFFER_SIZE 10
+#define ACC_DIFF_1_AND_2 1.5
+#define ACC_MAX 1.5
+#define ACC_DELTA_T 100000 //in microssec. deve ser <= ao intervalo de amostras da IMU
+#define TIME(a,b) ((a*1000000ull) + b)
+
 
 Quaternion<double> rotation(Vector3d theta) {
 
@@ -75,6 +91,138 @@ void alt_callback(const ardrone_autonomy::navdata_altitude& msg_in) {
     vz = -msg_in.altitude_vz*0.001;
 }
 
+double generateGaussianNoise(double mu, double sigma)
+{
+	const double epsilon = std::numeric_limits<double>::min();
+	const double two_pi = 2.0*3.14159265358979323846;
+
+	static double z0, z1;
+	static bool generate;
+	generate = !generate;
+
+	if (!generate)
+	   return z1 * sigma + mu;
+
+	double u1, u2;
+	do
+	 {
+	   u1 = rand() * (1.0 / RAND_MAX);
+	   u2 = rand() * (1.0 / RAND_MAX);
+	 }
+	while ( u1 <= epsilon );
+
+	z0 = sqrt(-2.0 * log(u1)) * cos(two_pi * u2);
+	z1 = sqrt(-2.0 * log(u1)) * sin(two_pi * u2);
+	return z0 * sigma + mu;
+}
+
+void getRawAcc(double ax, double ay, double az, Vector3d &acc1, Vector3d &acc2, unsigned long long &time_us) {
+
+    struct timeval time_sample;
+    acc1 = Vector3d(ax, ay, az) + Vector3d(generateGaussianNoise(0.0, 0.3), generateGaussianNoise(0.0, 0.3), generateGaussianNoise(0.0, 0.3));
+    acc2 = Vector3d(ax, ay, az) + Vector3d(generateGaussianNoise(0.0, 0.3), generateGaussianNoise(0.0, 0.3), generateGaussianNoise(0.0, 0.3));
+
+    gettimeofday(&time_sample, NULL);
+    time_us = TIME(time_sample.tv_sec,time_sample.tv_usec);
+}
+
+bool acceptDiffAccs(Vector3d acc1, Vector3d acc2) {
+
+    Vector3d diff(abs(acc1(0) - acc2(0)), abs(acc1(1) - acc2(1)), abs(acc1(2) - acc2(2)));
+
+    return (diff(0) <= ACC_DIFF_1_AND_2 && diff(1) <= ACC_DIFF_1_AND_2 && diff(2) <= ACC_DIFF_1_AND_2);
+
+
+}
+
+bool acceptMaxAcc(Vector3d acc) {
+
+    return (abs(acc(0)) <= ACC_MAX && abs(acc(1)) <= ACC_MAX && abs(acc(2)) <= ACC_MAX);
+}
+
+Vector3d doInterpolation(sample lastS, sample newS) {
+
+    f_vector_print("last: ", lastS.data);
+
+    Vector3d x =  (((newS.data - lastS.data) * ACC_DELTA_T )/ (newS.time_us - lastS.time_us)) + lastS.data;
+
+    return x;
+
+}
+
+
+void defineAcceleration(double ax, double ay, double az) {
+
+    //calibrate accelerometers
+
+    Vector3d acc1;
+    Vector3d acc2;
+    unsigned long long time;
+    getRawAcc(ax, ay, az, acc1, acc2, time);
+
+    if (!acceptDiffAccs(acc1, acc2)) { //eliminate if the difference between accs is big
+
+        cout << "nao aceitou diff" << endl;
+        return;
+    }
+
+    Vector3d acc = (acc1 + acc2) * 0.5;
+
+    if (!acceptMaxAcc(acc)) {
+        cout << "nao aceitou acc max" << endl;
+        return;
+    }
+
+
+    if (buffer_acc.empty()) { //first measurement
+        sample sp;
+        sp.data = acc;
+        sp.time_us = time;
+        buffer_acc.push_back(sp);
+        return;
+    }
+
+    sample newSample;
+    newSample.data = acc;
+    newSample.time_us = time;
+
+    for(deque<sample>::iterator it = buffer_acc.begin(); it != buffer_acc.end(); it++){
+        sample s = *it;
+        newSample.data += s.data;
+    }
+    newSample.data = newSample.data/(buffer_acc.size() + 1);
+
+    f_vector_print(" acc ", acc);
+    cout << " o time " << time << endl;
+
+    unsigned long long timeSample = buffer_acc.back().time_us + ACC_DELTA_T;
+
+    if (timeSample <= time) {
+
+        while (timeSample <= time) {
+            sample sp;
+            sp.data = doInterpolation(buffer_acc.back(), newSample);
+            sp.time_us = timeSample;
+            buffer_acc.push_back(sp);
+            timeSample += ACC_DELTA_T;
+        }
+    }
+
+    if (buffer_acc.size() == ACC_BUFFER_SIZE) {
+        buffer_acc.pop_front();
+    }
+
+
+    sample last = buffer_acc.back();
+
+    txt << last.data(0) <<";" << last.data(1)<<";" << last.data(2)<<";" << acc1(0)<<";" << acc1(1)<<";" << acc1(2)<<";" << acc2(0)<<";" << acc2(1)<<";" << acc2(2)<<";" << time <<"\n";
+
+}
+
+void definePosition(double ax, double ay, double az) {
+
+    defineAcceleration(ax, ay, az);
+}
 
 void nav_callback(const ardrone_autonomy::Navdata& msg_in)
 {
@@ -84,6 +232,10 @@ void nav_callback(const ardrone_autonomy::Navdata& msg_in)
     //do stuff
 
    float timestamp = msg_in.tm*0.000001;
+
+   definePosition(msg_in.ax, msg_in.ay, msg_in.az);
+
+
 
     //timestamp in microsecs
 
@@ -133,12 +285,12 @@ void nav_callback(const ardrone_autonomy::Navdata& msg_in)
 			f_vector_print("vel", vel);
 			f_vector_print("vll", velV);
 
-		
+
 			cout << "tm: " <<  timestamp << " dt: " << dt << endl;
 
 			f_vector_print("posg", x_new);
 			f_vector_print("posl", x_loc);
-	
+
 		}
 
                 txt << prefix_x;
@@ -205,9 +357,36 @@ void nav_callback(const ardrone_autonomy::Navdata& msg_in)
 int main(int argc, char **argv)
 {
 
-    txt.open("velocidade.txt");
+    txt.open("comp.txt");
 
-    ros::init(argc, argv, "pose");
+
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+    defineAcceleration(1.2, 1.0, 0.9); usleep(100000);
+
+
+    for(deque<sample>::iterator it = buffer_acc.begin(); it != buffer_acc.end(); it++){
+        sample sp = *it;
+        f_vector_print("acc sample", sp.data);
+        cout << " time: " << sp.time_us << endl;
+    }
+
+    txt.close();
+
+    /*ros::init(argc, argv, "pose");
 
     ros::NodeHandle n("~");
 
@@ -226,7 +405,7 @@ int main(int argc, char **argv)
     ros::Subscriber sub_alt = n.subscribe("/ardrone/navdata_altitude", 1, alt_callback);
 
 
-    ros::spin();
+    ros::spin();*/
 
     return 0;
 }
